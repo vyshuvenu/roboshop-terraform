46430ed8ab159727cdf84a9faa37ed30a6a06243
vyshuvenu <vyshuv1289@gmail.com>
2023-09-07 11:23:46 +0530

    dbs incluuded

--- main.tf
+++ main.tf
@@ -42,65 +42,65 @@ module "docdb" {
   instance_count          = each.value["instance_count"]
   instance_class          = each.value["instance_class"]
 }
-#
-#module "rds" {
-#  source = "git::https://github.com/vyshuvenu/tf-module-rds.git"
-#  tags   = var.tags
-#  env    = var.env
-#
-#  for_each = var.rds
-#
-#  subnet_ids              = local.db_subnets
-#  vpc_id                  = local.vpc_id
-#  sg_ingress_cidr         = local.app_subnets_cidr
-#  rds_type                = each.value["rds_type"]
-#  db_port                 = each.value["db_port"]
-#  engine_family           = each.value["engine_family"]
-#  engine                  = each.value["engine"]
-#  engine_version          = each.value["engine_version"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot     = each.value["skip_final_snapshot"]
-#  instance_count          = each.value["instance_count"]
-#  instance_class          = each.value["instance_class"]
-#}
-#
-#module "elasticache" {
-#  source = "git::https://github.com/vyshuvenu/tf-module-elasticache.git"
-#  tags   = var.tags
-#  env    = var.env
-#
-#  for_each = var.elasticache
-#
-#  subnet_ids       = local.db_subnets
-#  vpc_id           = local.vpc_id
-#  sg_ingress_cidr  = local.app_subnets_cidr
-#  elasticache_type = each.value["elasticache_type"]
-#  family           = each.value["family"]
-#  port             = each.value["port"]
-#  engine           = each.value["engine"]
-#  node_type        = each.value["node_type"]
-#  num_cache_nodes  = each.value["num_cache_nodes"]
-#  engine_version   = each.value["engine_version"]
-#}
-#
-#module "rabbitmq" {
-#  source  = "git::https://github.com/vyshuvenu/tf-module-rabbitmq.git"
-#  tags    = var.tags
-#  env     = var.env
-#  zone_id = var.zone_id
-#
-#  for_each = var.rabbitmq
-#
-#  subnet_ids       = local.db_subnets
-#  vpc_id           = local.vpc_id
-#  sg_ingress_cidr  = local.app_subnets_cidr
-#  instance_type    = each.value["instance_type"]
-#  ssh_ingress_cidr = var.ssh_ingress_cidr
-#}
+
+module "rds" {
+ source = "git::https://github.com/vyshuvenu/tf-module-rds.git"
+ tags   = var.tags
+ env    = var.env
+
+ for_each = var.rds
+
+ subnet_ids              = local.db_subnets
+ vpc_id                  = local.vpc_id
+ sg_ingress_cidr         = local.app_subnets_cidr
+ rds_type                = each.value["rds_type"]
+ db_port                 = each.value["db_port"]
+ engine_family           = each.value["engine_family"]
+ engine                  = each.value["engine"]
+ engine_version          = each.value["engine_version"]
+ backup_retention_period = each.value["backup_retention_period"]
+ preferred_backup_window = each.value["preferred_backup_window"]
+ skip_final_snapshot     = each.value["skip_final_snapshot"]
+ instance_count          = each.value["instance_count"]
+ instance_class          = each.value["instance_class"]
+}
+
+module "elasticache" {
+ source = "git::https://github.com/vyshuvenu/tf-module-elasticache.git"
+ tags   = var.tags
+ env    = var.env
+
+ for_each = var.elasticache
+
+ subnet_ids       = local.db_subnets
+ vpc_id           = local.vpc_id
+ sg_ingress_cidr  = local.app_subnets_cidr
+ elasticache_type = each.value["elasticache_type"]
+ family           = each.value["family"]
+ port             = each.value["port"]
+ engine           = each.value["engine"]
+ node_type        = each.value["node_type"]
+ num_cache_nodes  = each.value["num_cache_nodes"]
+ engine_version   = each.value["engine_version"]
+}
+
+module "rabbitmq" {
+  source  = "git::https://github.com/vyshuvenu/tf-module-rabbitmq.git"
+  tags    = var.tags
+  env     = var.env
+  zone_id = var.zone_id
+
+  for_each = var.rabbitmq
+
+  subnet_ids       = local.db_subnets
+  vpc_id           = local.vpc_id
+  sg_ingress_cidr  = local.app_subnets_cidr
+  instance_type    = each.value["instance_type"]
+  ssh_ingress_cidr = var.ssh_ingress_cidr
+}
 
 module "app" {
-  #depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
+  depends_on = [module.docdb, module.alb, module.elasticache, module.rabbitmq, module.rds]
   source = "git::https://github.com/vyshuvenu/tf-module-app.git"
 
   tags                    = merge(var.tags, each.value["tags"])
